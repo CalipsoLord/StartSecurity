3981cde8e007d0391a1a0bf4c03f219dfedb87fb
Gastón <gaston.e.prats@gmail.com>
2017-11-15 23:05:16 -0200

    Test projeto Spring

---
+++ StartSecurity/src/main/java/com/startsecurity/StartSecurity/StartSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.startsecurity.StartSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSecurityApplication.class, args);
+	}
+}

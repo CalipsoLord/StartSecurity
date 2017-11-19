3a21ca7703875ac7dd9bc16556fbef93f2150e69
Gastón <gaston.e.prats@gmail.com>
2017-11-19 13:37:00 -0200

    Simple hola mundo

--- StartSecurity/src/main/java/com/startsecurity/controler/IndexController.java
+++ StartSecurity/src/main/java/com/startsecurity/controler/IndexController.java
@@ -1,13 +1,13 @@
 package com.startsecurity.controler;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "index";
+		return "Hola mundo";
 	}
 }
--- StartSecurity/src/main/java/com/startsecurity/startsecurity/StartSecurityApplication.java
+++ StartSecurity/src/main/java/com/startsecurity/startsecurity/StartSecurityApplication.java
@@ -2,11 +2,21 @@ package com.startsecurity.startsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class StartSecurityApplication {
+public class StartSecurityApplication extends SpringBootServletInitializer{
+
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartSecurityApplication.class, args);
 	}
+	
 }

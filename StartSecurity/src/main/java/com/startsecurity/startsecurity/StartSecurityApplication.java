0b10ecbdac353233a7cd63a999c8742727f823fe
Gastón <gaston.e.prats@gmail.com>
2017-11-19 12:20:16 -0200

    test estrutura spring boot

--- StartSecurity/src/main/java/com/startsecurity/startsecurity/StartSecurityApplication.java
+++ StartSecurity/src/main/java/com/startsecurity/startsecurity/StartSecurityApplication.java
@@ -1,4 +1,4 @@
-package com.startsecurity.StartSecurity;
+package com.startsecurity.startsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
